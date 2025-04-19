dd6265656fb169f4615450bd0ff5ddfe1c7fe780
shankkarpal46 <shankkarpal46@gmail.com>
2025-04-19 08:57:53 +0530

    ngclass,ngstyle and servies

--- firstapp/src/app/attributedirectives/attributedirectives.component.ts
+++ firstapp/src/app/attributedirectives/attributedirectives.component.ts
@@ -1,16 +1,35 @@
+import { NgStyle } from '@angular/common';
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-attributedirectives',
-  imports: [NgClass],
+  imports: [NgClass,NgStyle],
   templateUrl: './attributedirectives.component.html',
   styleUrl: './attributedirectives.component.css'
 })
 export class AttributedirectivesComponent {
+  darktheme:boolean=true 
+  isHighlighted:boolean = true
   classes : any = {
     bg:"bg-warning",
     tc:"text-danger",
     m:"m-2"
+  }
+  
+  toggle(){
+    this.darktheme = !this.darktheme
+  }
+
+  boxStyles(){
+    return{
+      "background-color":this.isHighlighted?"red":"white",
+      "color":"green"
+    } 
+
+  }
+
+  changeHighlight(){
+    this.isHighlighted=!this.isHighlighted
   }
 }
